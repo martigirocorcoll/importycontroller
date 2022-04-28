f6aa1f8e95bd7ad67978d9523e449180cce165f8
Marti Giro <martigirocorcoll@gmail.com>
2022-04-28 20:02:18 +0200

    prestamos calculador devolucion

--- app/models/prestamo.rb
+++ app/models/prestamo.rb
@@ -30,4 +30,39 @@ class Prestamo < ApplicationRecord
     end
   end
 
+
+  def interesnominal
+    interesnominal = 0
+    restante = self.cantidad.to_i
+    self.flujos.each do |flujo|
+      unless flujo.fecha_efectiva == nil
+        dias = (flujo.fecha_efectiva - self.fecha).to_f
+        interesnominal += flujo.cantidad.to_i * (dias/365) * self.interes.to_i / 100
+        restante -= flujo.cantidad.to_i
+      end
+    end
+    dias_restantes = (Date.today - self.fecha).to_f
+    interesnominal += restante * (dias_restantes/365) * self.interes.to_i / 100
+
+    return interesnominal.to_i
+  end
+
+  def total_bruto
+    return self.cantidad.to_i + self.interesnominal
+  end
+
+  def retencion
+    return (self.interesnominal * 0.1596638655).to_i
+  end
+
+  def pendiente
+    suma = self.cantidad.to_i
+    self.flujos.each do |flujo|
+      unless flujo.fecha_efectiva == nil
+        suma += -flujo.cantidad.to_i
+      end
+    end
+    suma += self.interesnominal
+    suma += -self.retencion
+  end
 end
