81d413aaf0ef12730b98f4b67c137eea60b56d51
Marti Giro <martigirocorcoll@gmail.com>
2022-06-14 12:29:26 +0200

    order results in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @operacions = Operacion.all
+    @operacions = Operacion.order(id: :desc)
   end
 end
