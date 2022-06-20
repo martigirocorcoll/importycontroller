36405042c87fc19616f7cb03699c0861bdd00ea2
Marti Giro <martigirocorcoll@gmail.com>
2022-06-20 18:10:49 +0200

    pago total

--- app/models/gastomensual.rb
+++ app/models/gastomensual.rb
@@ -3,4 +3,9 @@ class Gastomensual < ApplicationRecord
   def self.basetotal
     return Gastomensual.all.pluck(:cantidad).sum
   end
+
+  def self.ivatotal
+    return Gastomensual.all.pluck(:iva).sum
+  end
+
 end
